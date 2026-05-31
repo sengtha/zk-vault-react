196d019f067f8b6c5988d92c8531c727d5d5c5f9
sengtha <sengtha@gmail.com>
2026-05-31 10:22:40 +0700

    Update hooks.ts

--- src/zk-vault/hooks.ts
+++ src/zk-vault/hooks.ts
@@ -1,9 +1,12 @@
 // src/zk-vault/hooks.ts
+
 import { useContext } from 'react';
 import { VaultContext } from './VaultContext';
 
 export function useZkVault() {
   const context = useContext(VaultContext);
-  if (!context) throw new Error('useZkVault must be used within VaultProvider');
+  if (!context) {
+    throw new Error('useZkVault must be used within a VaultProvider');
+  }
   return context;
 }
